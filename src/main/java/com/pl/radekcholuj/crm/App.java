48466c81c071396667bc3386eda32c47157f60ce
Radoslaw Choluj <radekcholuj@wp.pl>
2021-02-28 14:37:22 +0100

    #1 projekt mvn wygenerowany z archetypu

---
+++ src/main/java/com/pl/radekcholuj/crm/App.java
@@ -0,0 +1,13 @@
+package com.pl.radekcholuj.crm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
